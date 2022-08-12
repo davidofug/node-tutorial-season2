e4d3ae5ef73a76e6f4c70edf98d42a652d6befca
David Wampamba <davidwampamba@gmail.com>
2022-08-12 19:15:53 +0300

    session 1

---
+++ session1/sum-numbers.js
@@ -0,0 +1,14 @@
+const sum = (num1, num2) => {
+	if (typeof num1 !== "number" || typeof num2 !== "number") {
+		return "Invalid Input";
+	}
+	return num1 + num2;
+};
+
+let output = sum(1, 2);
+
+// console.log(output);
+
+output = sum("David", 3);
+
+console.log(output);
